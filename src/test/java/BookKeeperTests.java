a0729258352b0083654ed01c24d26ec90aa1bd41
Marcin Kapelan <194978@edu.p.lodz.pl>
2018-03-27 18:33:58 +0200

    Create empty BookKeeperTests

---
+++ src/test/java/BookKeeperTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class BookKeeperTests {
+
+}
